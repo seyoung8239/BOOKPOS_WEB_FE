57c535114a9e36f6dd3b6d72227e48b815e6308d
seyoung8239 <seyoung8239@gmail.com>
2022-08-16 14:35:25 +0900

    Feat: 도입절차 layout 구축

--- src/Components/PriceGuide/Screen5.tsx
+++ src/Components/PriceGuide/Screen5.tsx
@@ -13,9 +13,39 @@ const menuList = [
 
 const Estimate = (
 	<div>
-		<div>견적서 요청</div>
-		<div>bookpos@gmail.com</div>
-		<div>@bookpos</div>
+		<div
+			style={{
+				display: 'flex',
+				width: '250px',
+				justifyContent: 'space-between',
+			}}
+		>
+			<img
+				src="/img/estimate.png"
+				width="25px"
+				height="25px"
+				alt="intro1"
+			/>
+			<div style={{ fontWeight: 600, fontSize: '27px' }}>견적서 요청</div>
+			<img
+				src="/img/inqury.png"
+				width="90px"
+				height="25px"
+				alt="intro1"
+			/>
+		</div>
+		<div style={{ height: '40px' }}></div>
+		<div style={{ display: 'flex' }}>
+			<img src="/img/mail.png" width="25px" height="25px" alt="intro1" />
+			<div style={{ width: '10px' }}></div>
+			<div style={{ fontSize: '21px' }}>bookpos@gmail.com</div>
+		</div>
+		<div style={{ height: '20px' }}></div>
+		<div style={{ display: 'flex' }}>
+			<img src="/img/kakao.png" width="25px" height="25px" alt="intro1" />
+			<div style={{ width: '10px' }}></div>
+			<div style={{ fontSize: '21px' }}>@bookpos_</div>
+		</div>
 	</div>
 );
 
@@ -32,8 +62,21 @@ function Screen5() {
 					align-items: center;
 				`}
 			>
-				<div>도입절차</div>
-				<div>
+				<div
+					css={css`
+						font-size: 44px;
+						font-weight: 600;
+						margin-bottom: 10px;
+					`}
+				>
+					도입절차
+				</div>
+				<div
+					css={css`
+						margin-bottom: 40px;
+						color: #555;
+					`}
+				>
 					도입 절차의 각 단계에 대한 자세한 내용을 보시려면 각 버튼을
 					클릭해주세요.
 				</div>
@@ -42,11 +85,31 @@ function Screen5() {
 						display: flex;
 					`}
 				>
-					<div>
+					<div
+						css={css`
+							display: flex;
+							flex-direction: column;
+							justify-content: space-between;
+							height: 300px;
+						`}
+					>
 						{menuList.map((menu, i) => (
 							<div
 								css={css`
 									background-color: white;
+									padding: 5px;
+									border-radius: 3px;
+									height: 50px;
+									width: 150px;
+									font-weight: 600;
+									display: flex;
+									align-items: center;
+									border: ${i === menuIdx
+										? '1px solid black'
+										: ''};
+									// @media (min-width: 1200px) {
+									// 	width: 200px;
+									// }
 								`}
 								onClick={() => setMenuIdx(i)}
 							>
@@ -54,9 +117,16 @@ function Screen5() {
 							</div>
 						))}
 					</div>
+					<div style={{ width: '10px' }}></div>
 					<div
 						css={css`
 							background-color: white;
+							padding: 40px;
+							border-radius: 3px;
+							width: 35vw;
+							@media (min-width: 1200px) {
+								width: 30vw;
+							}
 						`}
 					>
 						{menuContents[menuIdx]}
